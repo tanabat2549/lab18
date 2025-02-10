fae3e056a1add0d26a8b0d8cc21c4031b41fb5b7
Thanabat <tanabat2549kuayjeng@gmail.com>
2025-02-11 00:27:59 +0700

    cp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1 , Rect r2){
+    double overlapX = max(r1.x, r2.x);
+    double overlapY = min(r1.y, r2.y);
+    double overlapWidth = min(r1.x + r1.w, r2.x + r2.w) - overlapX;
+    double overlapHeight = overlapY - max(r1.y - r1.h, r2.y - r2.h);
+    
+   
+    if (overlapWidth > 0 && overlapHeight > 0) {
+        
+        return overlapWidth * overlapHeight;
+    }
+    return 0;  
 }
+
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
-
-
-
+double overlap(const Rect* R1, const Rect* R2) {
+    double x_left = R1->x > R2->x ? R1->x : R2->x;
+    double y_top = R1->y < R2->y ? R1->y : R2->y;
+    double x_right = (R1->x + R1->w) < (R2->x + R2->w) ? (R1->x + R1->w) : (R2->x + R2->w);
+    double y_bottom = (R1->y - R1->h) > (R2->y - R2->h) ? (R1->y - R1->h) : (R2->y - R2->h);
+    double w = x_right - x_left;
+    double h = y_top - y_bottom;
+    
+    if (w > 0 && h > 0) {
+        return w * h;
+    } else {
+        return 0;
+    }
 }
